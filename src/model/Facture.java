682b17744fb09e9513f634f26da3f6f4b3db14e6
Loris Salvador <loris.salvador@student.hepl.be>
2023-10-05 15:04:53 +0200

    2eme commit

---
+++ src/model/Facture.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.Date;
+
+public class Facture {
+    private int idFacture;
+    private Date date;
+    private float montant;
+    private boolean paye;
+
+    public Facture(int id, Date d, float m, boolean p)
+    {
+        idFacture = id;
+        date = d;
+        montant = m;
+        paye = p;
+    }
+
+    public int getIdFacture() {
+        return idFacture;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public float getMontant() {
+        return montant;
+    }
+    public boolean getPaye() {
+        return paye;
+    }
+}
