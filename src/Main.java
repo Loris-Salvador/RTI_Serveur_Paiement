af7ef35c611d5a19dfef07ad7b7c4c51dc8ecb15
Loris Salvador <loris.salvador@student.hepl.be>
2023-10-05 15:06:43 +0200

    commit VESPAP

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,6 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            DatabaseConnection databaseConnection = new DatabaseConnection(DatabaseConnection.MYSQL, "localhost", "PSLA_RTI", "RTI", "RTI");
-        }
-        catch (SQLException e) {
-            System.out.println("SQL Exception");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        try {
-            System.out.println(DatabaseUseCase.isUsernameExists("Loris"));
-        }
-        catch (SQLException e) {
-            System.out.println("SQL Exception" + e.getMessage());
-        }
 
 
     }
